5ebaf8d472f908109fefda194f41c027ce37a24b
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-02-29 23:29:53 +0900

    誤字修正

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('register/pre_check', 'Auth\RegisterController@pre_check')->name('register.pre_check');
 //本会員登録入力
-Route::get('register/verify/{token}', 'Auth\RegisterController@showFrom');
+Route::get('register/verify/{token}', 'Auth\RegisterController@showForm');
 Route::post('register/main_check', 'Auth\RegisterController@mainCheck')->name('register.main.check');
 Route::post('register/main_register', 'Auth\RegisterController@mainRegister')->name('register.main.registered');
