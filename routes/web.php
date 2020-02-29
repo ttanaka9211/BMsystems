51a1c726da60e825866648ea779180608d64d4a8
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-02-29 10:17:36 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Auth::routes(['verify' => true]);
+Route::get('/', 'HomeController@index');
 
-Route::get('/home', 'HomeController@index')->name('home');
-
+/* ログイン、ログアウト、パスワード変更、パスワード変更（忘れた時） */
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::post('register/pre_check', 'Auth\RegisterController@pre_check')->name('register.pre_check');
-Route::get('register/verify/{token}', 'Auth\RegisterController@shoFrom');
+//本会員登録入力
+Route::get('register/verify/{token}', 'Auth\RegisterController@showFrom');
 Route::post('register/main_check', 'Auth\RegisterController@mainCheck')->name('register.main.check');
 Route::post('register/main_register', 'Auth\RegisterController@mainRegister')->name('register.main.registered');
