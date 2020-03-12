d1854d9c739e7d0d1a9db070d4c9d88bafca0255
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-03-12 23:39:44 +0900

    修正

--- app/Http/Controllers/Admin/BaseShift/Ajax/UserAcceptController.php
+++ app/Http/Controllers/Admin/BaseShift/Ajax/UserAcceptController.php
@@ -1,11 +1,28 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Baseshift\Ajax;
+namespace App\Http\Controllers\Admin\BaseShift\Ajax;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Mail\UserAccept;
+use Illuminate\Support\Facades\Mail;
 
 class UserAcceptController extends Controller
 {
-    //
+    public function index()
+    {
+        $query = \App\Models\BaseShift::query();
+
+        return $query->get();
+    }
+
+    public function accept(Request $request)
+    {
+        $user = \App\Models\BaseShift::find($request->user_id);
+        $user->accepted = $request->accept;
+        $result = $user->save();
+        $email = new UserAccept($user);
+        Mail::to($user->email)->send($email);
+        return ['result' => $result];
+    }
 }
