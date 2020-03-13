bb6b779ab3b087acd0adf300854b8f9585f0882d
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-03-14 00:13:44 +0900

    request_shiftsマイグレーション

---
+++ database/migrations/2020_03_14_000444_create_shifts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_shifts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('email');
+            $table->integer('week_id');
+            $table->integer('timezone_id');
+            $table->boolean('accepted')
+                ->default(false)
+                ->comment('管理者承認');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_shifts');
+    }
+}
