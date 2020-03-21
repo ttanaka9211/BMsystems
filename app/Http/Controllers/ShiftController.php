6c9bc2f7030aba14d8ef2b90ded6ffa85f70ac7f
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-03-22 00:54:34 +0900

    日付入力

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -6,8 +6,6 @@ use App\Models\BaseShift;
 use Illuminate\Http\Request;
 use App\Models\Shift;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection\orderBy;
-
 
 class ShiftController extends Controller
 {
@@ -18,7 +16,6 @@ class ShiftController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
@@ -28,35 +25,42 @@ class ShiftController extends Controller
      */
     public function create(Request $request)
     {
+        //require __DIR__.'/../vender/autoload.php';
         //当月の日数
-        $start = Carbon::now()->addWeeks()->startOfWeek()->toDateString();
-        $end = Carbon::now()->addWeeks()->endOfWeek()->toDateString();
-        echo $start;
-        $dateArray = array();
-        $cnt = 0;
-        for ($i = $start; $i <= $end; $i = date(date('Y-m-d', strtotime($i . '+1 day')))) {
-            array_push($dateArray, $i . ',' . ++$cnt);
-        }
-        dump($dateArray);
-
-        $idArray = array();
-        //$builder = BaseShift::select(['id', 'user_id', 'week_id', 'timezone_id'])->get();
-        //dump($builder);
-        $builder = BaseShift::get(['id', 'week_id', 'timezone_id', 'user_id']);
-        $idArray = $builder->toArray();
-        dump($idArray);
-        $idArray = (object) $idArray;
-        dump($idArray);
-        $weeks = $idArray->id;
-        damp($weeks);
-        // foreach ($weeks as $week) {
-        //     $user_id = $array->user_id;
-        //     $week_id = $array->week_id;
-        //     $timezone_id = $array->timezone_id;
-        //     $data[$i] = ['user_id' => $user_id, 'week_id' => $week_id, 'timezone_id' => $timezone_id];
-        //     ++$i;
+        $start = Carbon::now()->addWeeks()->startOfWeek(); //->toDateString();
+        $end = Carbon::now()->addWeeks()->endOfWeek(); //->toDateString();
+        // $dateArray = array();
+        // $cnt = 0;
+        // for ($i = $start; $i <= $end; $i = date(date('Y-m-d', strtotime($i.'+1 day')))) {
+        //     array_push($dateArray, $i.','.++$cnt);
         // }
-        // dump($data);
+        // dump($dateArray);
+        dump($start->dayOfWeek);
+        dump($start->toDateString());
+
+        dump($end->dayOfWeekIso);
+        $i = 0;
+        $weeks = BaseShift::get(['user_id', 'week_id', 'timezone_id']);
+        foreach ($weeks as $week) {
+            $user_id = $week->user_id;
+            $week_id = $week->week_id;
+            $timezone_id = $week->timezone_id;
+            $data[$i] = [
+                'user_id' => $user_id,
+                'week_id' => $week_id,
+                'timezone_id' => $timezone_id,
+
+                'date'=>(
+                if($week_id==$start->dayOfWeekIso){
+                    $start->toDateString();
+                } else{
+                    date('Y-m-d', strtotime($start.'+1 day'));
+                })
+            ];
+            ++$i;
+        }
+        dump($data);
+
         return view('shift.test');
         // return view('shift.test', compact($dateArray));
     }
@@ -64,56 +68,56 @@ class ShiftController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
     }
 }
--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
 {
-    //
+    protected $fillable = ['user_id', 'timezone_id', 'week_id'];
 }
